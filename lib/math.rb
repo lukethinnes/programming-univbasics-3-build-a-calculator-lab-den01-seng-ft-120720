ca508c8a195aa2a1fb4075efda9d7c72877c3941
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-15 21:01:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  var adds = num1 += num2
+  puts add
 end
 
 def subtraction(num1, num2)
-
+  var sub = num1 -= num2
+  puts sub
 end
 
 def division(num1, num2)
-
+  var divide = num1 /= num2
+  puts divide
 end
 
 def multiplication(num1, num2)
-
+  var times = num1 *= num2
+  puts times
 end
 
 def modulo(num1, num2)
-
+  var mod = num1 %= num2
+  puts mod
 end
 
 def square_root(num)
-
+  var sqrt = Math.sqrt(num)
+  puts sqrt
 end
